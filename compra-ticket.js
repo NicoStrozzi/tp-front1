607f4558affe2f3ecc54c81ea18ac523e1d27bd5
nicostrozzi <nicostrozzi@hotmail.com>
2023-05-27 20:19:12 -0300

    segunda parte

---
+++ compra-ticket.js
@@ -0,0 +1,30 @@
+function calcular(event){ 
+ event.preventDefault();
+  
+  const nombre=document.getElementById('nombre').value;
+     
+  const apellido=document.getElementById('apellido').value;
+   
+  const correo=document.getElementById('correo').value;
+     
+  const cantidad=document.getElementById('cantidad').value;
+     
+  const categoria=document.getElementById('categoria').value;
+      
+  if(!nombre || !apellido  || !correo  || !cantidad || !categoria ){
+    alert("Complete los campos");   
+  }
+
+    if(categoria==='estudiante'){
+      total.value=parseFloat(cantidad)*200*0.2;
+    }else{
+      if(categoria==='trainee'){
+        total.value=parseFloat(cantidad)*200*0.5;
+      }else{
+        total.value=parseFloat(cantidad)*200*0.85;
+      }
+  }
+}
+
+var btn_calcular=document.getElementById('btn_calcular');
+btn_calcular.addEventListener('click',calcular);
